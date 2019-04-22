d2610e83c6514295626fc7109a353237d0132afa
Apoorva Shetty <apshetty@uw.edu>
2019-04-21 17:29:25 -0700

    Adding more functions to the module

--- modules/importData.py
+++ modules/importData.py
@@ -1,6 +1,19 @@
 import numpy as np
 import pandas as pd
 
-def importData(dataLink, seperator = ','):
-    df = pd.read_csv(dataLink,sep=seperator)
-    return df
+##Function that reads data from an online data source
+def readData(dataLink,separator = ','):
+    return pd.read_csv(dataLink,sep = separator)
+
+##Function to verify column count is atleast 3
+def checkColumns(dataFrame):
+    if(dataFrame.shape[1]<3):
+        return False
+    return True
+
+##Function that creates a dataframe from read data, and returns a dataframe if column count >=3, otherwise returns null
+def importData(dataLink, separator = ','):
+    df = readData(dataLink,separator)
+    if(checkColumns(df)):
+        return df
+    return None
